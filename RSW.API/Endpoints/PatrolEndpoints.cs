8216d74a2c35f7799cdd1a52a93de36b6c7fb092
Kasper <95905774+k-brok@users.noreply.github.com>
2025-09-19 09:00:47 +0200

    Added endpoints

---
+++ RSW.API/Endpoints/PatrolEndpoints.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RSW.API.Data;
+using RSW.Shared.Dto;
+using RSW.Shared.Entities;
+using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace RSW.API.Endpoints
+{
+    public static class PatrolEndpoints
+    {
+        public static void MapPatrolEndpoints(this WebApplication app)
+        {
+            var group = app.MapGroup("/api/activities").WithTags("Patrol");
+
+            group.MapGet("/", async (AppDbContext Context) =>
+            {
+                var all = await Context.Patrols.ToListAsync();
+                return Results.Ok(all.Select(A => A.ToDto()));
+            }).AllowAnonymous();
+
+            group.MapGet("/{id:guid}", async (Guid id, AppDbContext Context) =>
+            {
+                var patrol = await Context.Patrols.FirstOrDefaultAsync(A => A.Id == id);
+                return patrol is not null ? Results.Ok(patrol.ToDto()) : Results.NotFound();
+            }).AllowAnonymous();
+
+            group.MapPost("/", async (PatrolDto patrol, AppDbContext Context) =>
+            {
+                var created = Context.Patrols.Add(patrol.ToEntity());
+                await Context.SaveChangesAsync();
+                return Results.Created($"/api/activities/{created.Entity.Id}", created);
+            });
+
+            group.MapPut("/{id:guid}", async (Guid id, PatrolDto updated, AppDbContext Context) =>
+            {
+                Patrol? result = await Context.Patrols.FirstOrDefaultAsync(A => A.Id == id);
+                if (result == null)
+                    return Results.NotFound();
+
+                result.Id = updated.Id;
+                result.Name = updated.Name;
+                result.Number = updated.Number;
+                result.SubGroupId = updated.SubGroupId;
+                result.GroupId = updated.GroupId;
+                result.TotalScore = updated.TotalScore;
+                result.position = updated.position;
+                result.IsYoungest = updated.IsYoungest;
+
+                await Context.SaveChangesAsync();
+                return Results.Ok(result);
+            });
+
+            group.MapDelete("/{id:guid}", async (Guid id, AppDbContext Context) =>
+            {
+                var FoundPatrol = await Context.Patrols.FirstOrDefaultAsync(A => A.Id == id);
+                if (FoundPatrol == null)
+                    return Results.NotFound();
+
+                var success = Context.Patrols.Remove(FoundPatrol);
+                await Context.SaveChangesAsync();
+                return Results.Ok();
+            });
+        }
+    }
+}
--- RSW.API/Program.cs
+++ RSW.API/Program.cs
@@ -1,6 +1,6 @@
-
-using JVVApp.API.Data;
 using Microsoft.EntityFrameworkCore;
+using RSW.API.Data;
+using RSW.API.Endpoints;
 
 namespace RSW.API;
 
@@ -36,25 +36,7 @@ public class Program
 
         app.UseAuthorization();
 
-        //var summaries = new[]
-        //{
-        //    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        //};
-
-        //app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-        //{
-        //    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        //        new WeatherForecast
-        //        {
-        //            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-        //            TemperatureC = Random.Shared.Next(-20, 55),
-        //            Summary = summaries[Random.Shared.Next(summaries.Length)]
-        //        })
-        //        .ToArray();
-        //    return forecast;
-        //})
-        //.WithName("GetWeatherForecast")
-        //.WithOpenApi();
+        app.MapPatrolEndpoints();
 
         app.Run();
     }
