651383ac2780a1381da1a540e87766093404965a
Kasper <95905774+k-brok@users.noreply.github.com>
2025-09-19 20:29:45 +0200

    Added Migration Drop Codes

---
+++ RSW.API/Migrations/20250919182821_UpdateEntities.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RSW.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Code",
+                table: "SignupCodes");
+
+            migrationBuilder.DropColumn(
+                name: "Code",
+                table: "JurySlots");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Code",
+                table: "SignupCodes",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Code",
+                table: "JurySlots",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
