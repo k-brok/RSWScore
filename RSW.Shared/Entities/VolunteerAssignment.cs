c42a6a2baac4f46ac749eb93ce93d720f2bbf758
Kasper Brok <kasperbrok@gmail.com>
2025-09-24 16:08:01 +0200

    Fixed infinite loop issue

--- RSW.Shared/Entities/VolunteerAssignment.cs
+++ RSW.Shared/Entities/VolunteerAssignment.cs
@@ -1,11 +1,16 @@
+using System.Text.Json.Serialization;
+
 namespace RSW.Shared.Entities
 {
     public class VolunteerAssignment : BaseEntity
     {
+        [JsonIgnore]
         public ApplicationUser? User { get; set; }
         public string? UserId { get; set; }
+        [JsonIgnore]
         public Edition? Edition { get; set; }
         public Guid EditionId { get; set; }
+        [JsonIgnore]
         public VolunteerTask? Task { get; set; }
         public Guid TaskId { get; set; }
         public bool HasVOG { get; set; } = false;
