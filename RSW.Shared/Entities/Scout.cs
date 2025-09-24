9a34b0f78d65dc9942eacf1b9c32460b4a1e92ef
Kasper Brok <kasperbrok@gmail.com>
2025-09-24 09:54:56 +0200

    Move Calculation functions to Dto

--- RSW.Shared/Entities/Scout.cs
+++ RSW.Shared/Entities/Scout.cs
@@ -11,36 +11,6 @@ namespace RSW.Shared.Entities
         public bool IsAPL { get; set; } = false;
         public Patrol Patrol { get; set; } = null!;
         public required Guid PatrolId { get; set; }
-        [NotMapped]
-        public int Age
-        {
-            get
-            {
-                DateOnly today = DateOnly.FromDateTime(DateTime.Today);
-
-                int age = today.Year - DateOfBirth.Year;
-
-                // Corrigeren als de verjaardag dit jaar nog niet is geweest
-                if (today < new DateOnly(today.Year, DateOfBirth.Month, DateOfBirth.Day))
-                {
-                    age--;
-                }
-
-                return age;
-            }
-        }
-        public int CalculateAge(DateOnly Date)
-        {
-
-            int age = Date.Year - DateOfBirth.Year;
-
-            // Corrigeren als de verjaardag dit jaar nog niet is geweest
-            if (Date < new DateOnly(Date.Year, DateOfBirth.Month, DateOfBirth.Day))
-            {
-                age--;
-            }
-
-            return age;
-        }
+        
     }
 }
