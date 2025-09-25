ae8559ecd86584bfcb5391644a89ae4211b04a26
Kasper Brok <kasperbrok@gmail.com>
2025-09-25 09:30:00 +0200

    Removed Dto

--- RSW.Shared/Entities/Patrol.cs
+++ RSW.Shared/Entities/Patrol.cs
@@ -1,11 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
 namespace RSW.Shared.Entities
 {
     public class Patrol : BaseEntity
     {
-        public required string Name { get; set; }
+        [Required]
+        public string Name { get; set; }
         public int? Number { get; set; } = null;
         public Guid? SubGroupId { get; set; } = null;
         [JsonIgnore]
@@ -21,6 +23,35 @@ namespace RSW.Shared.Entities
         public bool IsYoungest { get; set; } = false;
         [JsonIgnore]
         public List<Scout> Scouts { get; set; } = new List<Scout>();
-        
+        public bool unranked(DateOnly LSWDate)
+        {
+            if (Scouts.Count < 5)
+                return true;
+
+            if (Scouts.Count > 7)
+                return true;
+
+            int NrScoutsAge15 = 0;
+
+            foreach (Scout scout in Scouts)
+            {
+                if (scout.CalculateAge(LSWDate) < 11)
+                    return true;
+
+                if (scout.CalculateAge(LSWDate) > 15)
+                    return true;
+
+                if (scout.CalculateAge(LSWDate) == 15)
+                    NrScoutsAge15++;
+            }
+
+            if (Scouts.Count == 5 && NrScoutsAge15 > 2)
+                return true;
+            
+            if (NrScoutsAge15 > 3)
+                return true;
+
+            return false;
+        }
     }
 }
--- RSW.Shared/Entities/Scout.cs
+++ RSW.Shared/Entities/Scout.cs
@@ -17,6 +17,35 @@ namespace RSW.Shared.Entities
         public Patrol Patrol { get; set; } = null!;
         [Required]
         public Guid PatrolId { get; set; }
-        
+        public int Age
+        {
+            get
+            {
+                DateOnly today = DateOnly.FromDateTime(DateTime.Today);
+
+                int age = today.Year - DateOfBirth.Year;
+
+                // Corrigeren als de verjaardag dit jaar nog niet is geweest
+                if (today < new DateOnly(today.Year, DateOfBirth.Month, DateOfBirth.Day))
+                {
+                    age--;
+                }
+
+                return age;
+            }
+        }
+        public int CalculateAge(DateOnly Date)
+        {
+
+            int age = Date.Year - DateOfBirth.Year;
+
+            // Corrigeren als de verjaardag dit jaar nog niet is geweest
+            if (Date < new DateOnly(Date.Year, DateOfBirth.Month, DateOfBirth.Day))
+            {
+                age--;
+            }
+
+            return age;
+        }
     }
 }
