6ef10c98b7439e93303b1374dec68c130f7ce3d0
Kasper Brok <kasperbrok@gmail.com>
2025-09-25 09:18:09 +0200

    Moved to required Attributes

--- RSW.Shared/Entities/Scout.cs
+++ RSW.Shared/Entities/Scout.cs
@@ -1,18 +1,22 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
 namespace RSW.Shared.Entities
 {
     public class Scout : BaseEntity
     {
-        public required string Firstname { get; set; }
-        public required string Lastname { get; set; }
+        [Required]
+        public string? Firstname { get; set; }
+        [Required]
+        public string? Lastname { get; set; }
         public DateOnly DateOfBirth { get; set; }
         public bool IsPL { get; set; } = false;
         public bool IsAPL { get; set; } = false;
         [JsonIgnore]
         public Patrol Patrol { get; set; } = null!;
-        public required Guid PatrolId { get; set; }
+        [Required]
+        public Guid PatrolId { get; set; }
         
     }
 }
